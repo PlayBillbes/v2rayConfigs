ee30c53388c8db9a4332ab12d4689cd1bcfdcba5
PlayBillbes <131470638+PlayBillbes@users.noreply.github.com>
2024-06-22 16:16:42 +0630

    Update cluster-mods.sh

--- cluster-mods.sh
+++ cluster-mods.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/PlayBillbes/v2rayConfigs/main/cluster.sh
-chmod +x cluster.sh
-nohup ./cluster.sh >/dev/null 2>&1 &
-sleep 10
-echo $(cat list.log)
-
+rm -f web config.json
+wget -O temp.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip temp.zip
+sleep 2
+rm -f temp.zip
+sleep 2
+mv v2ray web
+sleep 2
+rm -rf config.json
+sleep 2
+wget https://raw.githubusercontent.com/PlayBillbes/render-me/main/config.json
+sleep 2
+wget https://github.com/eooce/choreo-2go/raw/main/files/server
+sleep 5
+chmod +x web server
+sleep 5
+nohup ./web run &>/dev/null &
+sleep 5
+./server tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --url http://localhost:8008
