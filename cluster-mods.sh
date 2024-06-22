8d9f76cc09d1d9c57ee80749d11147b066aa366e
PlayBillbes <131470638+PlayBillbes@users.noreply.github.com>
2024-06-22 16:05:11 +0630

    Update cluster-mods.sh

--- cluster-mods.sh
+++ cluster-mods.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/PlayBillbes/v2rayConfigs/main/cluster.sh
 chmod +x cluster.sh
-nohup ./cluster.sh
+nohup ./cluster.sh &>/dev/null
 echo $(cat list.log)
 
