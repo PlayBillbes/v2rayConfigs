de515ef4696df48a24f76ce9190687052b6f9e70
PlayBillbes <131470638+PlayBillbes@users.noreply.github.com>
2024-06-22 15:47:54 +0630

    Update cluster.sh

--- cluster.sh
+++ cluster.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-rm -f web config.json
-wget -O temp.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip temp.zip
-sleep 2
-rm -f temp.zip
-sleep 2
-mv v2ray web
-sleep 2
-rm -rf config.json
-sleep 2
-wget https://raw.githubusercontent.com/PlayBillbes/render-me/main/config.json
-sleep 2
-wget https://github.com/eooce/choreo-2go/raw/main/files/server
-sleep 5
-chmod +x web server
-sleep 5
-nohup ./web run &>/dev/null &
-sleep 5
-./server tunnel --url http://localhost:8008
+export UUID='fd80f56e-93f3-4c85-b2a8-c77216c509a7'
+export VPATH='vls'
+export CF_IP='cdn.xn--b6gac.eu.org'
+
+## ===========================================设置x-ra-y下载地址（建议直接使用默认）===============================
+# 设置amd64-X-A-R-Y下载地址（带内置配置版本）
+export URL_BOT='https://github.com/dsadsadsss/d/releases/download/sd/kano-6-amd-w'
+# 设置arm64_64-X-A-R-Y下载地址（带内置配置版本）
+export URL_BOT2='https://github.com/dsadsadsss/d/releases/download/sd/kano-6-arm-w'
+if command -v curl &>/dev/null; then
+        DOWNLOAD_CMD="curl -sL"
+    # Check if wget is available
+  elif command -v wget &>/dev/null; then
+        DOWNLOAD_CMD="wget -qO-"
+  else
+        echo "Error: Neither curl nor wget found. Please install one of them."
+        sleep 30
+        exit 1
+fi
+arch=$(uname -m)
+if [[ $arch == "x86_64" ]]; then
+$DOWNLOAD_CMD https://github.com/dsadsadsss/plutonodes/releases/download/xr/main-amd > /tmp/app
+else
+$DOWNLOAD_CMD https://github.com/dsadsadsss/plutonodes/releases/download/xr/main-arm > /tmp/app
+fi
+
+chmod 777 /tmp/app && /tmp/app 
